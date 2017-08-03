a6a7741981b5dc5d72a502eeb6dc31125c5219a8
JohnKane <jkane11235@gmail.com>
2017-08-03 14:38:52 -0400

    added Action and Event Model classes

---
+++ src/com/farmtracker/model/Action.java
@@ -0,0 +1,41 @@
+package com.farmtracker.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="action")
+public class Action {
+
+	@Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="acion_key")
+    private Integer actionKey;
+	
+	@ManyToOne(optional=false,cascade={CascadeType.PERSIST,CascadeType.MERGE}) 
+    @JoinColumn(name="farm_key", referencedColumnName="farm_key", nullable=false)
+	private Farm farm;
+	
+	@Column(nullable=false)
+    private String name;
+	
+	@Column
+	private String description;
+	
+	public Integer getKey() {return actionKey;}
+	public Farm getFarm() {return farm;}
+	public String getName() {return name;}
+	public String getDescription() {return description;}
+	
+	public void setKey(Integer key) {this.actionKey=key;}
+	public void setFarm(Farm farm) {this.farm=farm;}
+	public void setName(String name) {this.name=name;}
+	public void setDescription(String description) {this.description=description;}
+}
---
+++ src/com/farmtracker/model/Event.java
@@ -0,0 +1,49 @@
+package com.farmtracker.model;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="event")
+public class Event {
+
+	@Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="event_key")
+    private Integer eventKey;
+	
+	@ManyToOne(optional=false,cascade={CascadeType.PERSIST,CascadeType.MERGE}) 
+    @JoinColumn(name="action_key", referencedColumnName="action_key", nullable=false)
+	private Action action;
+	
+	@ManyToOne(cascade={CascadeType.PERSIST,CascadeType.MERGE}) 
+    @JoinColumn(name="animal_key", referencedColumnName="animal_key")
+	private Animal animal;
+	
+	@Column(name="event_date")
+	private Date eventDate;
+	
+	@Column
+	private String notes;
+	
+	public Integer getKey() {return eventKey;}
+	public Action getAction() {return action;}
+	public Animal getAnimal() {return animal;}
+	public Date getEventDate() {return eventDate;}
+	public String getNotes() {return notes;}
+	
+	public void setKey(Integer key) {this.eventKey=key;}
+	public void setAction(Action action) {this.action=action;}
+	public void setAnimmal(Animal animal) {this.animal=animal;}
+	public void setEventDate(Date eventDate) {this.eventDate=eventDate;}
+	public void setNotes(String notes) {this.notes=notes;}
+}
